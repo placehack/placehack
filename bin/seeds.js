d8bb8a5113bb2754663831b4525c1e81f54617b1
Marina Cid Reyes <ymaryna@MacBook-Pro-de-Marina.local>
2019-12-14 14:52:48 +0100

    new seeds

--- bin/seeds.js
+++ bin/seeds.js
@@ -5,140 +5,152 @@ require('../config/db.config')
 const dbtitle = 'destinations';
 mongoose.connect(`mongodb://localhost/${dbtitle}`);
 
-// Place.deleteMany()
-// .then(data => {
-    // console.log('ELIMINADOS' + data)
-// })
-// .catch(error => console.error(error))
+Place.deleteMany()
+.then(data => {
+    console.log('ELIMINADOS' + JSON.stringify(data))
 
-
-const places = [
-    {
-        name: 'Barcelona',
-        mainPhoto: '/img/mainPhotos/barcelona.jpg',
-        descriptionPhoto: '/img/descriptionPhotos/madrid.jpg',
-        location: {
-            lat: 39.4699075,
-            lng: -0.3762881,
-            country: 'Spain',
-            continent: 'Europe',
-        },
-        images: [
-            
-        ],
-        info: {
-            description: 'Is a city on the coast of northeastern Spain. It is the capital and largest city of the autonomous community of Catalonia, as well as the second most populous municipality of Spain. With a population of 1.6 million within city limits,[7] its urban area extends to numerous neighbouring municipalities within the Province of Barcelona and is home to around 4.8 million people,[3][8] making it the sixth most populous urban area in the European Union after Paris, London, Madrid, the Ruhr area and Milan.[3] It is one of the largest metropolises on the Mediterranean Sea, located on the coast between the mouths of the rivers Llobregat and Besòs, and bounded to the west by the Serra de Collserola mountain range, the tallest peak of which is 512 metres (1,680 feet) high.',
-            hotSpots: [
-                {
-                    name: 'n',
-                    location: {
-                        lat: 0,
-                        lng: 0,
-                    },
-                    images: [
-
-                    ],
-                    description: 'd',
-                }
-            ]
-        },
-
-        filters: {
-            time: "Weekend" ,
-            language: "Spanish",
-            activities: "Active tourism",
-            money: "€€",
-        }
-    },
-    {
-        name: 'Madrid',
-        mainPhoto: '/img/mainPhotos/madrid.jpg',
-        descriptionPhoto: '/img/descriptionPhotos/barcelona.jpg',
-        location: {
-            lat: 39.4699075,
-            lng: -0.3762881,
-            country: 'Spain',
-            continent: 'Europe',
-        },
-        images: [
-            
-        ],
-        info: {
-            description: 'Is a city on the coast of northeastern Spain. It is the capital and largest city of the autonomous community of Catalonia, as well as the second most populous municipality of Spain. With a population of 1.6 million within city limits,[7] its urban area extends to numerous neighbouring municipalities within the Province of Barcelona and is home to around 4.8 million people,[3][8] making it the sixth most populous urban area in the European Union after Paris, London, Madrid, the Ruhr area and Milan.[3] It is one of the largest metropolises on the Mediterranean Sea, located on the coast between the mouths of the rivers Llobregat and Besòs, and bounded to the west by the Serra de Collserola mountain range, the tallest peak of which is 512 metres (1,680 feet) high.',
-            hotSpots: [
-                {
-                    name: 'n',
-                    location: {
-                        lat: 0,
-                        lng: 0,
-                    },
-                    images: [
-
-                    ],
-                    description: 'd',
-                }
-            ]
-        },
-
-        filters: {
-            time: "Weekend" ,
-            language: "Spanish",
-            activities: "Active tourism",
-            money: "€€",
-        }
-    },
-
-    {
-        name: 'Tokio',
-        mainPhoto: '/img/mainPhotos/barcelona.jpg',
-        descriptionPhoto: '/img/descriptionPhotos/madrid.jpg',
-        location: {
-            lat: 39.4699075,
-            lng: -0.3762881,
-            country: 'Japan',
-            continent: 'Asia',
-        },
-        images: [
-            
-        ],
-        info: {
-            description: 'Is a city on the coast of northeastern Spain. It is the capital and largest city of the autonomous community of Catalonia, as well as the second most populous municipality of Spain. With a population of 1.6 million within city limits,[7] its urban area extends to numerous neighbouring municipalities within the Province of Barcelona and is home to around 4.8 million people,[3][8] making it the sixth most populous urban area in the European Union after Paris, London, Madrid, the Ruhr area and Milan.[3] It is one of the largest metropolises on the Mediterranean Sea, located on the coast between the mouths of the rivers Llobregat and Besòs, and bounded to the west by the Serra de Collserola mountain range, the tallest peak of which is 512 metres (1,680 feet) high.',
-            hotSpots: [
-                {
-                    name: 'n',
-                    location: {
-                        lat: 0,
-                        lng: 0,
-                    },
-                    images: [
-
-                    ],
-                    description: 'd',
-                }
-            ]
-        },
-
-        filters: {
-            time: "+Week" ,
-            language: "Japanese",
-            activities: "Active tourism",
-            money: "€€€",
-        }
-    },
-]
-
-const createPlaces = places.map(place => {
+    const places = [
+        {
+            name: 'Barcelona',
+            mainPhoto: '/img/mainPhotos/barcelona.jpg',
+            descriptionPhoto: '/img/descriptionPhotos/madrid.jpg',
+            location: {
+                lat: 39.4699075,
+                lng: -0.3762881,
+                country: 'Spain',
+                continent: 'Europe',
+            },
+            images: [
+                
+            ],
+            info: {
+                description: 'Is a city on the coast of northeastern Spain. It is the capital and largest city of the autonomous community of Catalonia, as well as the second most populous municipality of Spain. With a population of 1.6 million within city limits,[7] its urban area extends to numerous neighbouring municipalities within the Province of Barcelona and is home to around 4.8 million people,[3][8] making it the sixth most populous urban area in the European Union after Paris, London, Madrid, the Ruhr area and Milan.[3] It is one of the largest metropolises on the Mediterranean Sea, located on the coast between the mouths of the rivers Llobregat and Besòs, and bounded to the west by the Serra de Collserola mountain range, the tallest peak of which is 512 metres (1,680 feet) high.',
+                hotSpots: [
+                    {
+                        name: 'n',
+                        location: {
+                            lat: 0,
+                            lng: 0,
+                        },
+                        images: [
     
-    const newPlace = new Place(place)
-    return newPlace.save()
-        .then(place => {
-            console.log(place)
-        })
-        .catch(error => {
-            console.log(error)
-            throw new Error(`Impossible to add the place. ${error}`)
-        })
+                        ],
+                        description: 'd',
+                    }
+                ]
+            },
+    
+            filters: {
+                time: "Weekend" ,
+                language: "Spanish",
+                activities: "Active tourism",
+                money: "€€",
+            }
+        },
+        {
+            name: 'Madrid',
+            mainPhoto: '/img/mainPhotos/madrid.jpg',
+            descriptionPhoto: '/img/descriptionPhotos/barcelona.jpg',
+            location: {
+                lat: 39.4699075,
+                lng: -0.3762881,
+                country: 'Spain',
+                continent: 'Europe',
+            },
+            images: [
+                
+            ],
+            info: {
+                description: 'Is a city on the coast of northeastern Spain. It is the capital and largest city of the autonomous community of Catalonia, as well as the second most populous municipality of Spain. With a population of 1.6 million within city limits,[7] its urban area extends to numerous neighbouring municipalities within the Province of Barcelona and is home to around 4.8 million people,[3][8] making it the sixth most populous urban area in the European Union after Paris, London, Madrid, the Ruhr area and Milan.[3] It is one of the largest metropolises on the Mediterranean Sea, located on the coast between the mouths of the rivers Llobregat and Besòs, and bounded to the west by the Serra de Collserola mountain range, the tallest peak of which is 512 metres (1,680 feet) high.',
+                hotSpots: [
+                    {
+                        name: 'n',
+                        location: {
+                            lat: 0,
+                            lng: 0,
+                        },
+                        images: [
+    
+                        ],
+                        description: 'd',
+                    }
+                ]
+            },
+    
+            filters: {
+                time: "Weekend" ,
+                language: "Spanish",
+                activities: "Active tourism",
+                money: "€€",
+            }
+        },
+    
+        {
+            name: 'Tokio',
+            mainPhoto: '/img/mainPhotos/barcelona.jpg',
+            descriptionPhoto: '/img/descriptionPhotos/madrid.jpg',
+            location: {
+                lat: 39.4699075,
+                lng: -0.3762881,
+                country: 'Japan',
+                continent: 'Asia',
+            },
+            images: [
+                
+            ],
+            info: {
+                description: 'Is a city on the coast of northeastern Spain. It is the capital and largest city of the autonomous community of Catalonia, as well as the second most populous municipality of Spain. With a population of 1.6 million within city limits,[7] its urban area extends to numerous neighbouring municipalities within the Province of Barcelona and is home to around 4.8 million people,[3][8] making it the sixth most populous urban area in the European Union after Paris, London, Madrid, the Ruhr area and Milan.[3] It is one of the largest metropolises on the Mediterranean Sea, located on the coast between the mouths of the rivers Llobregat and Besòs, and bounded to the west by the Serra de Collserola mountain range, the tallest peak of which is 512 metres (1,680 feet) high.',
+                hotSpots: [
+                    {
+                        name: 'n',
+                        location: {
+                            lat: 0,
+                            lng: 0,
+                        },
+                        images: [
+    
+                        ],
+                        description: 'd',
+                    }
+                ]
+            },
+    
+            filters: {
+                time: "+Week" ,
+                language: "Japanese",
+                activities: "Active tourism",
+                money: "€€€",
+            }
+        },
+    ]
+    
+    const createPlaces = places.map(place => {
+        
+        const newPlace = new Place(place)
+        return newPlace.save()
+            .then(place => {
+                console.log(place)
+            })
+            .catch(error => {
+                console.log(error)
+                throw new Error(`Impossible to add the place. ${error}`)
+            })
+    })
+    return Promise.all(createPlaces)
+    .then(
+        data => {
+            return data;
+        }
+    )
+    .catch(error => {
+        console.log(error)
+        throw new Error(`Nuse k pasa. ${error}`)
+    })
 })
+.catch(error => console.error(error))
+.then(()=>process.exit(0))
+
+
 
 // {
 //     name,
